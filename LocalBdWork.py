f52e53e4535ad9c15288c0ea687970f216c24544
kirka50 <75656501+kirka50@users.noreply.github.com>
2022-06-25 22:16:22 +0500

    /

--- LocalBdWork.py
+++ LocalBdWork.py
@@ -1,22 +1,33 @@
 import sqlite3
 import json
+import pymysql
+from psycopg import sql
+
+
 
 class LocalBd:
-    def createMySqlConnection(self):
+    host = "127.0.0.1"
+    user = "Popa"
+    password = "200218kirill"
+    db_name = "smartinv"
+    BdTags = ["ВремяСуток", "ВремяГода", "Местность", "Авиа", "Автомобили", "БПЛА", "Водолаз", "Кинолог", "Кони", "Обьятия", "Шерп"]
+
+    def createMySqlConnection(self,host = "127.0.0.1",user = "Popa",password = "200218kirill",db_name = "smartinv"):
         try:
-            sqlite_connection = sqlite3.connect('sqlite_python.db')
-            cursor = sqlite_connection.cursor()
-            print("База данных создана и успешно подключена к SQLite")
+            connection = pymysql.connect(
+                host=host,
+                port=3306,
+                user=user,
+                password=password,
+                database=db_name,
+                cursorclass=pymysql.cursors.DictCursor
+            )
+            print("Соединение установлено")
+            return connection
 
-            sqlite_select_query = "select sqlite_version();"
-            cursor.execute(sqlite_select_query)
-            record = cursor.fetchall()
-            print("Версия базы данных SQLite: ", record)
-
-        except sqlite3.Error as error:
-            print("Ошибка при подключении к sqlite", error)
-
-        return sqlite_connection
+        except Exception as ex:
+            print("Jopa")
+            print(ex)
 
 
     def CreateCursorFromConnection(self,connection):
@@ -26,32 +37,111 @@ class LocalBd:
         except sqlite3 as ex:
             print(ex)
 
+
     def createNewTable(self):
         connection = self.createMySqlConnection()
         cursor = self.CreateCursorFromConnection(connection)
-        cursor.execute('''CREATE TABLE sorted_media_with_tags(mediaUrl TEXT NOT NULL, mediaType TEXT NOT NULL, Tags TEXT NOT NULL)''')
+        cursor.execute('''CREATE TABLE sorted_media_with_tags(mediaUrl VARCHAR(500) NOT NULL PRIMARY KEY, Tags TEXT, FULLTEXT KEY findTag (Tags))''')
         cursor.close()
-        self.CloseConnection(connection)
+        self.closeConnection(connection)
 
 
-
-    def checkSqlVersion(self,connection):
-        cursor = self.CreateCursorFromConnection(connection)
-        sqlite_select_query = "select sqlite_version();"
-        cursor.execute(sqlite_select_query)
-        record = cursor.fetchall()
-        print("Версия базы данных SQLite: ", record)
-
-
-    def CloseConnection(self,connection):
+    def closeConnection(self, connection):
         connection.close()
         print("Соединенее закрыто")
-#class ListWorker:
 
+
+    def selectRows(self):
+        connection = self.createMySqlConnection()
+        cursor = self.CreateCursorFromConnection(connection)
+        cursor.execute('''SELECT * FROM `sorted_media_with_tags`''')
+        record = cursor.fetchall()
+        self.closeConnection(connection)
+        return record
+
+
+    def dropTable(self):
+        connection = self.createMySqlConnection()
+        cursor = self.CreateCursorFromConnection(connection)
+        cursor.execute('''DROP TABLE `sorted_media_with_tags`''')
+        self.closeConnection(connection)
+
+
+    def findImageNameByTag(self,findTag):
+        connection = self.createMySqlConnection()
+        cursor = self.CreateCursorFromConnection(connection)
+        cursor.execute('''SELECT mediaUrl FROM sorted_media_with_tags where match (Tags) AGAINST("%s")''',findTag)
+        images = cursor.fetchall()
+        self.closeConnection(connection)
+        return images
+
+
+    def findImagesNameByTwoTag(self, findTag, findTag2):
+        connection = self.createMySqlConnection()
+        cursor = self.CreateCursorFromConnection(connection)
+        cursor.execute('''SELECT mediaUrl FROM sorted_media_with_tags where match (Tags) AGAINST ("+%s*+%s*")''',(findTag,findTag2))
+        images = cursor.fetchall()
+        self.closeConnection(connection)
+        return images
+
+    def findImagesNameByThreeTag(self, findTag, findTag2, findTag3):
+        connection = self.createMySqlConnection()
+        cursor = self.CreateCursorFromConnection(connection)
+        cursor.execute('''SELECT mediaUrl FROM sorted_media_with_tags where match (Tags) AGAINST ("+%s*+%s*+%s*")''',(findTag,findTag2,findTag3))
+        images = cursor.fetchall()
+        self.closeConnection(connection)
+        return images
+
+
+    def getTagsByMediaName(self,mediaName):
+        connection = self.createMySqlConnection()
+        cursor = self.CreateCursorFromConnection(connection)
+        cursor.execute('''SELECT Tags FROM `sorted_media_with_tags` WHERE mediaUrl = %s''',mediaName)
+        Tags = cursor.fetchone()
+        self.closeConnection(connection)
+        return Tags
+
+
+    def addNewTagToTagsOfMedia(self,tag,mediaName):
+        connection = self.createMySqlConnection()
+        cursor = self.CreateCursorFromConnection(connection)
+        tagsDict = self.getTagsByMediaName(mediaName)
+        print(tagsDict)
+        appendedDict = self.appendTagToList(tag, tagsDict)
+        print(appendedDict)
+        self.pasteTagsByMediaName(appendedDict,mediaName)
+
+
+
+    def appendTagToList(self, tag, tagsDict = {}):
+        tagsDict["Tags"].append(tag)
+        return tagsDict
+
+
+    def pasteTagsByMediaName(self,tags,mediaName):
+        connection = self.createMySqlConnection()
+        cursor = self.CreateCursorFromConnection(connection)
+        cursor.execute("UPDATE sorted_media_with_tags SET Tags = %s WHERE mediaUrl = %s",(tags,mediaName))
+        connection.commit()
+        self.closeConnection(connection)
+
+
+    def addNewMediaAndTag(self,mediaName,Tags):
+        connection = self.createMySqlConnection()
+        cursor = self.CreateCursorFromConnection(connection)
+        cursor.execute("INSERT INTO sorted_media_with_tags (mediaUrl,Tags) VALUES (%s,%s)", (mediaName, Tags))
+        connection.commit()
+        self.closeConnection(connection)
 
 
 
 
 Worker = LocalBd()
-Worker.createNewTable()
+work = Worker.getTagsByMediaName('Аываывац')
+print(work)
+Worker.addNewTagToTagsOfMedia('киса','Аываывац')
+#Worker.dropTable()
+#Worker.createNewTable()
+
+
 
